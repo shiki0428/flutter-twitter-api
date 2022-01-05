dae476bede638b6c85b2c508111cdb1d8131e035
Toshiki Shikina <e185725@ie.u-ryukyu.ac.jp>
2022-01-05 17:43:02 +0900

    タイムラインの表示は可能

---
+++ android/app/src/main/kotlin/com/example/twitter_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twitter_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:oauth1/oauth1.dart' as oauth1;
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  final controller = TextEditingController();
+  final platform = oauth1.Platform(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/authorize',
+    'https://api.twitter.com/oauth/access_token',
+    oauth1.SignatureMethods.hmacSha1,
+  );
+  final clientCredentials = oauth1.ClientCredentials(
+    'api',
+    'api secret',
+  );
+  late final auth = oauth1.Authorization(clientCredentials, platform);
+  oauth1.Credentials? tokenCredentials;
+  late String body = "null";
+  @override
+  void initState() {
+    super.initState();
+
+    // CallbackURLを"oob"とすることでPINでの認証とできる
+    auth.requestTemporaryCredentials('oob').then((res) {
+      tokenCredentials = res.credentials;
+      // launch() で ログイン用URLを開く
+      launch(auth.getResourceOwnerAuthorizationURI(tokenCredentials!.token));
+    });
+  }
+
+  void changeState(val) {
+    setState(() {
+      body = val;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ログイン後に表示されたPINを入力
+            TextFormField(
+              controller: controller,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // 入力されたPINを元に Access Token を取得
+                final pin = controller.text;
+
+                print(pin);
+                final verifier = pin;
+                print(tokenCredentials);
+                print(1);
+                final res = await auth.requestTokenCredentials(
+                  tokenCredentials!,
+                  verifier,
+                );
+                print('Access Token: ${res.credentials.token}');
+                print('Access Token Secret: ${res.credentials.tokenSecret}');
+
+                // 取得した Access Token を使ってAPIにリクエストできる
+                final client = oauth1.Client(
+                  platform.signatureMethod,
+                  clientCredentials,
+                  res.credentials,
+                );
+                print(client);
+                final apiResponse = await client.get(
+                  Uri.parse(
+                      'https://api.twitter.com/1.1/statuses/home_timeline.json?count=1'),
+                );
+
+                changeState(apiResponse.body[0]);
+                print(apiResponse.body);
+              },
+              child: Text('OK'),
+            ),
+
+            Text(
+              body,
+              softWrap: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
